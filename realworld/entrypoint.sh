ae90dd8103518dd002898339a0c6c330203a93ec
tuyojr <olutuyod@gmail.com>
2023-05-29 08:36:56 +0100

    testing @danjac 's conduit realworld app

--- realworld/entrypoint.sh
+++ realworld/entrypoint.sh
@@ -2,6 +2,7 @@
 source venv/bin/activate
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 pip install -r requirements.txt
+python manage.py makemigrations accounts
 SECRET_KEY=$(python -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())")
 ./manage.py migrate
 ./manage.py runserver 0.0.0.0:8000
